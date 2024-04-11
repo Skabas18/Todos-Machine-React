69b609f7d54214c3580427b5efb587da2e30e56a
Sebastian Cabal <sebascabalq@gmail.com>
2024-04-11 08:15:08 -0500

    Creación de lógica para que espere a que cargue el localstorage para poder editar el todo

--- src/routes/edit/EditTodoPage.js
+++ src/routes/edit/EditTodoPage.js
@@ -4,16 +4,24 @@ import { useTodos } from '../useTodos';
 import { useParams } from 'react-router-dom';
 
 function EditTodoPage() {
-  const params =useParams();
-  const {stateUpdaters} =useTodos();
-  const {editTodo} = stateUpdaters
-  return (
-    <TodoForm
-      label='Edita tu TODO'
-      submitText='Editar'
-      submitEvent={(newText) => editTodo(params.id, newText)}
-    />
-  )
+  const params = useParams();
+  const { state, stateUpdaters } = useTodos();
+  const { loading, getTodo } = state;
+  const { editTodo } = stateUpdaters;
+
+  if (loading) {
+    return <p>Cargando...</p>
+  } else {
+    const todo = getTodo(params.id);
+    return (
+      <TodoForm
+        label='Edita tu TODO'
+        defaultTodoText={todo.text}
+        submitText='Editar'
+        submitEvent={(newText) => editTodo(params.id, newText)}
+      />
+    )
+  }
 }
 
 export { EditTodoPage }
