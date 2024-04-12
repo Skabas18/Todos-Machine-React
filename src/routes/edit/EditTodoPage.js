d4e6c664af74ff7b3fdd637dde9165d51c30a105
Sebastian Cabal <sebascabalq@gmail.com>
2024-04-12 07:29:40 -0500

    Creación de logica para mostrar el texto por defecto del todo a modificar y creacion de la logica para que persista la información y no espere la ruta si no se refresca la pagina

--- src/routes/edit/EditTodoPage.js
+++ src/routes/edit/EditTodoPage.js
@@ -1,27 +1,35 @@
 import React from 'react'
 import { TodoForm } from '../../ui/TodoForm/index';
 import { useTodos } from '../useTodos';
-import { useParams } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 
 function EditTodoPage() {
+  const location = useLocation();
   const params = useParams();
   const { state, stateUpdaters } = useTodos();
   const { loading, getTodo } = state;
   const { editTodo } = stateUpdaters;
 
-  if (loading) {
+  let todoText;
+  
+  if (location.state?.todo) {
+    todoText = location.state.todo.text;
+  } else if (loading) {
     return <p>Cargando...</p>
   } else {
     const todo = getTodo(params.id);
-    return (
-      <TodoForm
-        label='Edita tu TODO'
-        defaultTodoText={todo.text}
-        submitText='Editar'
-        submitEvent={(newText) => editTodo(params.id, newText)}
-      />
-    )
+    todoText = todo.text;
   }
+
+
+  return (
+    <TodoForm
+      label='Edita tu TODO'
+      defaultTodoText={todoText}
+      submitText='Editar'
+      submitEvent={(newText) => editTodo(params.id, newText)}
+    />
+  )
 }
 
 export { EditTodoPage }
